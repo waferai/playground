20b471ddbbd0c9d3bbba46a2fdfd1d8d71da8727
rkyadav-ncsu <rkyadav@ncsu.edu>
2022-02-05 17:34:54 -0500

    refactoring

--- fine_tune_bert.py
+++ fine_tune_bert.py
@@ -1,4 +1,5 @@
 import os
+from sre_parse import Tokenizer
 import numpy as np
 import tensorflow as tf
 
@@ -38,18 +39,32 @@ class BertTrainer:
 
         # for key, value in glue_train.items():
         #     print(f"{key:9s}: {value[0].numpy()}")
-        
-        self.__process_tokenizer()
+        # load tokenizer
+        self.__load_tokenizer()
 
-        test_sentence = 'breastfeeding'
-        print(self.tokenizer.tokenize(test_sentence))
-
-    def __process_tokenizer(self):
+    def __load_tokenizer(self):
         # Set up tokenizer to generate Tensorflow dataset
         self.tokenizer = bert.tokenization.FullTokenizer(vocab_file=os.path.join(self.gs_folder_bert, "vocab.txt"),do_lower_case=True)
+        print("Vocab size:", len(self.tokenizer.vocab))
+
+    def __run_tokenizer(self,text_str):
+        if not self.tokenizer:
+            return None
+        tokens =  self.tokenizer.tokenize(text_str)
+        return self.tokenizer.convert_tokens_to_ids(tokens)
+
+    def run_inference(self, input_arr):
+        result = []
+        for i in input_arr:
+            i_token = self.__run_tokenizer(i)
+            result.append(i_token)
+        
+        return result
 
 
 
 if __name__ == "__main__":
     bert_trainer = BertTrainer()
+    input_arr = ['this is a test','i love my country','I hate <mask>','i hate germany']
+    print(bert_trainer.run_inference(input_arr=input_arr))
     print('ran')
